221e377736aafe66070c8ca8cfa9f30ae8ee48fb
Martin Rybak <martin.rybak@yodle.com>
2016-10-24 22:13:20 -0400

    Added conversion tests for integer types

--- SQLClient/SQLClientTests/SQLClientTests.m
+++ SQLClient/SQLClientTests/SQLClientTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "SQLClient.h"
 
 @interface SQLClientTests : XCTestCase
 
@@ -14,21 +15,91 @@
 
 @implementation SQLClientTests
 
-- (void)setUp
+- (void)testBit
 {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+	XCTestExpectation* expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+	[self execute:@"SELECT Bit FROM Test" completion:^(NSArray* results) {
+		XCTAssertEqualObjects(results[0][0][@"Bit"], [NSNull null]);
+		XCTAssertEqualObjects(results[0][1][@"Bit"], @(1));
+		XCTAssertEqualObjects(results[0][2][@"Bit"], @(0));
+		[expectation fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:[SQLClient sharedInstance].timeout handler:nil];
 }
 
-- (void)tearDown
+- (void)testTinyInt
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+	XCTestExpectation* expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+	[self execute:@"SELECT TinyInt FROM Test" completion:^(NSArray* results) {
+		XCTAssertEqualObjects(results[0][0][@"TinyInt"], [NSNull null]);
+		XCTAssertEqualObjects(results[0][1][@"TinyInt"], @(0));
+		XCTAssertEqualObjects(results[0][2][@"TinyInt"], @(255));
+		[expectation fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:[SQLClient sharedInstance].timeout handler:nil];
 }
 
-- (void)testExample
+- (void)testSmallInt
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+	XCTestExpectation* expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+	[self execute:@"SELECT SmallInt FROM Test" completion:^(NSArray* results) {
+		XCTAssertEqualObjects(results[0][0][@"SmallInt"], [NSNull null]);
+		XCTAssertEqualObjects(results[0][1][@"SmallInt"], @(-32768));
+		XCTAssertEqualObjects(results[0][2][@"SmallInt"], @(32767));
+		[expectation fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:[SQLClient sharedInstance].timeout handler:nil];
+}
+
+- (void)testInt
+{
+	XCTestExpectation* expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+	[self execute:@"SELECT Int FROM Test" completion:^(NSArray* results) {
+		XCTAssertEqualObjects(results[0][0][@"Int"], [NSNull null]);
+		XCTAssertEqualObjects(results[0][1][@"Int"], @(-2147483648));
+		XCTAssertEqualObjects(results[0][2][@"Int"], @(2147483647));
+		[expectation fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:[SQLClient sharedInstance].timeout handler:nil];
+}
+
+- (void)testBigInt
+{
+	XCTestExpectation* expectation = [self expectationWithDescription:NSStringFromSelector(_cmd)];
+	[self execute:@"SELECT BigInt FROM Test" completion:^(NSArray* results) {
+		XCTAssertEqualObjects(results[0][0][@"BigInt"], [NSNull null]);
+		XCTAssertEqualObjects(results[0][1][@"BigInt"], @((signed long long)-9223372036854775808));
+		XCTAssertEqualObjects(results[0][2][@"BigInt"], @((signed long long)9223372036854775807));
+		[expectation fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:[SQLClient sharedInstance].timeout handler:nil];
+}
+
+#pragma mark - Private
+
+- (void)execute:(NSString*)sql completion:(void (^)(NSArray* results))completion
+{
+	NSDictionary* environment = [[NSProcessInfo processInfo] environment];
+	SQLClient* client = [SQLClient sharedInstance];
+	
+	//Create environment variables in the Test Debug Scheme
+	NSString* host = environment[@"HOST"];
+	NSString* username = environment[@"USERNAME"];
+	NSString* password = environment[@"PASSWORD"];
+	NSString* database = environment[@"DATABASE"];
+	
+	NSParameterAssert(host);
+	NSParameterAssert(username);
+	NSParameterAssert(password);
+	
+	[client connect:host username:username password:password database:database completion:^(BOOL success) {
+		[client execute:sql completion:^(NSArray* results) {
+			[client disconnect];
+			if (completion) {
+				completion(results);
+			}
+		}];
+	}];
 }
 
 @end
